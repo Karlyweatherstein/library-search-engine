568b492429c2c6adeb41e6ecb8f1bc43e220aa48
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-06-28 16:16:08 -0400

    debugging

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -18,7 +18,7 @@ const SavedBooks = () => {
   // use this to determine if `useEffect()` hook needs to run again
   const { loading, data } = useQuery(GET_ME);
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
-  const useData = data?.me;
+  const userData = data?.me;
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
